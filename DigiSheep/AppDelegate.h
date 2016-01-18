8f2d35f2da8e1b255dd43ea1efd3ffa3d9210236
Tomn <tmnstudios@hotmail.fr>
2016-01-18 08:54:56 +0100

    Initial Commit

---
+++ DigiSheep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DigiSheep
+//
+//  Created by Tomn on 18/01/2016.
+//  Copyright © 2016 Tomn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DigiSheep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DigiSheep
+//
+//  Created by Tomn on 18/01/2016.
+//  Copyright © 2016 Tomn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
